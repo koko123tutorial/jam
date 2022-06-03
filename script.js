62b3b58ca7f66fbc3a0b3faa03e133b23b1a6994
koko123tutorial <koko123tutorial@gmail.com>
2022-06-03 10:30:15 +0700

    add sound

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 setInterval(() => {
     let date = new Date()
     let clock = document.getElementById('jam')
+    var audio = new Audio("suarajam.mp3");
     clock.innerHTML =
         date.getHours()+":"+
         date.getMinutes()+":"+
         date.getSeconds()
+        audio.play();
 }, 1000);
